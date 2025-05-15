e4ea82c8c8615924652a75154be9a836a26104ff
kidkrub <kritsadamoungkhao@gmail.com>
2025-05-15 03:40:34 -0700

    fix manifest

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,36 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       devOptions: { enabled: true },
+      manifest: {
+        name: "Grocery List PWA",
+        short_name: "GLP",
+        description: "Simple Grocery List that can work offline",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
     tailwindcss(),
   ],
